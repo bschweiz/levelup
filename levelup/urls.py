f1398e912f6c441a72558ae9535aee0426152e44
Benjamin Schweizer <bschweiz@gmail.com>
2021-02-16 20:47:11 -0600

    /profile endpoint works well and has been tested in Postman, now need to transition over to configure the React App

--- levelup/urls.py
+++ levelup/urls.py
@@ -17,12 +17,13 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
 from levelupapi.views import register_user,login_user
-from levelupapi.views import GameTypes, Games, Events
+from levelupapi.views import GameTypes, Games, Events, Profiles
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypes, 'gametype')
 router.register(r'games', Games, 'game')
 router.register(r'events', Events, 'event')
+router.register(r'profile', Profiles, 'profile')
 
 urlpatterns = [
     path('', include(router.urls)),
