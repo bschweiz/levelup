a427b1ac6188c53fbecbc1bbe5ef9fc2bc3cec8c
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-01 13:23:34 -0600

    definining the GameTests class

--- tests/game_tests.py
+++ tests/game_tests.py
@@ -2,3 +2,18 @@ import json
 from rest_framework import status
 from rest_framework.test import APITestCase
 from levelupai.models import GameType
+
+class GameTests(APITestCase):
+    def setUp(self):
+        #create new account and sample category
+        url = '/register'
+        data = = {
+            'username': 'steve',
+            'password': 'Admin8*',
+            'email': 'steve@stevebrownlee.com',
+            'address': '100 Infinity Way',
+            'phone_number': '555-1212',
+            'first_name': 'Steve',
+            'last_name': 'Brownlee',
+            'bio': 'Love those gamez!!'
+        }
